dd3d1818901ff984bc40202c7f148e61a5384ab5
joas-barros <joas.barros14@hotmail.com>
2024-10-05 20:11:42 -0300

    criando primeiras tabelas

---
+++ app/db/comandos.sql
@@ -0,0 +1,18 @@
+CREATE TABLE Efeito (
+	id SERIAL PRIMARY KEY,
+	nome varchar(30),
+	informacao varchar(200)
+);
+
+CREATE TABLE Tipo (
+	id SERIAL PRIMARY KEY,
+	nome varchar(30)
+);
+
+CREATE TABLE Habilidade (
+	id SERIAL PRIMARY KEY,
+	nome varchar(30),
+	tipo_habilidade int references tipo(id),
+	descricao varchar(200),
+	efeito_habilidade int references efeito(id)
+);
